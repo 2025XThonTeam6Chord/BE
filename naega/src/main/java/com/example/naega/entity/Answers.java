328d5b1a3ae8790e18b78249481eeb0b54b5a209
LEESIWOO <swlee4796@naver.com>
2025-11-23 02:47:24 +0900

    프롬프트 수정

--- naega/src/main/java/com/example/naega/entity/Answers.java
+++ naega/src/main/java/com/example/naega/entity/Answers.java
@@ -9,6 +9,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -16,6 +17,8 @@ import lombok.NoArgsConstructor;
 @Entity
 @Getter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "answers")
 public class Answers {
     @Id
