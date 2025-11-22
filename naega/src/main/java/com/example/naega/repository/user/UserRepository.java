f14afa9713e4bc971f2ebdc01429e087ec37caef
LEESIWOO <swlee4796@naver.com>
2025-11-23 02:52:35 +0900

    UsersRepository.java 삭제

--- naega/src/main/java/com/example/naega/repository/user/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.naega.repository.user;
-
-import com.example.naega.entity.Users;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<Users, Long> {
-
-
-}
