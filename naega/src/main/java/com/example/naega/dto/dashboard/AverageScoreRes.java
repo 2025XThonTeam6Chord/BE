d44304916be47fbf6ed32acb903ae24f2222479e
JIEUNI <jieundaeun@naver.com>
2025-11-22 20:51:58 +0900

    [feat] #5 AverageScoreRes 구현

---
+++ naega/src/main/java/com/example/naega/dto/dashboard/AverageScoreRes.java
@@ -0,0 +1,24 @@
+package com.example.naega.dto.dashboard;
+
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+import java.util.List;
+
+@Schema(description = "평균 심리 점수 추이 DTO")
+public record AverageScoreRes(
+        @ArraySchema(
+                schema = @Schema(description = "주간 평균 심리 점수 리스트"),
+                arraySchema = @Schema(implementation = AverageScore[].class)
+        )
+        List<AverageScore> averageScores
+) {
+    @Schema(description = "심리 점수 DTO")
+    public record AverageScore(
+            @Schema(description = "X축(날짜)", example = "2025/08/17~2025/08/24")
+            String dateX,
+
+            @Schema(description = "Y축(점수)", example = "28")
+            String scoreY
+    ){}
+}
