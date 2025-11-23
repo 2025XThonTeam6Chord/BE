8f660139f2bc85366e56674d0a46612385e8f06b
eundeang <111877048+eundeang@users.noreply.github.com>
2025-11-23 12:16:51 +0900

    [CONFIG] cors 허용 오리진 추가

--- naega/src/main/java/com/example/naega/config/SecurityConfig.java
+++ naega/src/main/java/com/example/naega/config/SecurityConfig.java
@@ -38,7 +38,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:5173", "chrome-extension://cdhepfchdehlobigcaafcekogjegejjg"));
+        configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:5173", "chrome-extension://*"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
